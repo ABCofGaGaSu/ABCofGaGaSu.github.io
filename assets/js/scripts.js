057b55d945bc2e69aa626c1ab73b9611c8ddc023
yuchen071 <90603179+yuchen071@users.noreply.github.com>
2021-12-17 17:59:24 +0800

    carousel test
    Custom indicators and controls
    Not finished

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,3 +5,11 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+$('.myindicator').on('slid.bs.carousel', function (event) {
+    var nextactiveslide = $(event.relatedTarget).index();
+    var $btns = $('.carousel-buttons');
+    var $active = $btns.find("[data-slide-to='" + nextactiveslide + "']");
+    $btns.find('.img-circle').removeClass('active');
+    $active.find('.img-circle').addClass('active');
+});
